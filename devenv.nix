e3880306e8e1d2a734f76613d4c1a41a0a078e35
David Rhoads <davidrhoads@mailstack.com>
2025-05-01 08:26:38 -0400

    revert from dev plugins

--- devenv.nix
+++ devenv.nix
@@ -3,14 +3,15 @@
   languages.rust.enable = true;
 
   # https://devenv.sh/packages/
-  packages = with pkgs; [
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base.dev
-    gst_all_1.gst-plugins-good
-    gtk4
-    libadwaita
-    openssl
-  ];
+packages = with pkgs; [
+  gst_all_1.gstreamer
+  gst_all_1.gst-plugins-base
+  gst_all_1.gst-plugins-good
+  gtk4
+  libadwaita
+  openssl
+];
+
 
   # See full reference at https://devenv.sh/reference/options/
 }
