b2ed094c3fec1824e0c9ad8b625350fa176a2d06
David Rhoads <davidrhoads@mailstack.com>
2025-05-08 11:48:32 -0400

    fix build errors

--- devenv.nix
+++ devenv.nix
@@ -1,47 +1,41 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, ... }: {
   languages.rust.enable = true;
 
   packages = with pkgs; [
+    # Base GStreamer packages
     gst_all_1.gstreamer
+    gst_all_1.gstreamer.dev
+    
+    # GStreamer plugins
     gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-base.dev
     gst_all_1.gst-plugins-good
     gst_all_1.gst-plugins-bad
     gst_all_1.gst-plugins-ugly
     gst_all_1.gst-libav
+    
+    # Additional GStreamer components needed
+    gst_all_1.gst-plugins-bad.dev
+    gst_all_1.gst-plugins-base.dev
+    
+    # Audio plugins specifically
+    gst_all_1.gst-plugins-base.out
+    
+    # GTK and other dependencies
     gtk4
     libadwaita
     openssl
     pkg-config
-    gst_all_1.gstreamer.dev
-    gst_all_1.gst-plugins-base.dev
-    gst_all_1.gst-plugins-good.dev
-    gst_all_1.gst-plugins-bad.dev
-    gst_all_1.gst-plugins-ugly.dev
-    gst_all_1.gst-libav.dev
-    gst_all_1.gst-plugins-base.out
-    gst_all_1.gst-plugins-good.out
-    gst_all_1.gst-plugins-bad.out
-    gst_all_1.gst-plugins-ugly.out
-    gst_all_1.gst-libav.out
-    gst_all_1.gst-plugins-base.dev.out
-    gst_all_1.gst-plugins-base.dev.dev
-    gst_all_1.gst-plugins-base.dev.lib
   ];
 
-  env.PKG_CONFIG_PATH = lib.makeSearchPathOutput "dev" "lib/pkgconfig" [
-    pkgs.gst_all_1.gstreamer.dev
-    pkgs.gst_all_1.gst-plugins-base.dev
-    pkgs.gst_all_1.gst-plugins-good.dev
-    pkgs.gst_all_1.gst-plugins-bad.dev
-    pkgs.gst_all_1.gst-plugins-ugly.dev
-    pkgs.gst_all_1.gst-libav.dev
-    pkgs.gst_all_1.gst-plugins-base.dev.out
-    pkgs.gst_all_1.gst-plugins-base.dev.dev
-    pkgs.gst_all_1.gst-plugins-base.dev.lib
-    pkgs.gst_all_1.gst-plugins-base.out
-    pkgs.gst_all_1.gst-plugins-good.out
-    pkgs.gst_all_1.gst-plugins-bad.out
-    pkgs.gst_all_1.gst-plugins-ugly.out
-    pkgs.gst_all_1.gst-libav.out
+  env.PKG_CONFIG_PATH = with pkgs; lib.makeSearchPath "lib/pkgconfig" [
+    gst_all_1.gstreamer
+    gst_all_1.gstreamer.dev
+    gst_all_1.gst-plugins-base.dev
+    gst_all_1.gst-plugins-base.out
+    gst_all_1.gst-plugins-bad.dev
+    gtk4
+    libadwaita
+    openssl.dev
   ];
 }
